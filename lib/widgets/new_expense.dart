6fb32214d41bc7b877668f29cd87d299fac18b1c
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-09-25 09:39:18 +0530

    cancel button added

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,20 @@ class NewExpense extends StatefulWidget{
 }
 
 class _NewExpenseState extends State<NewExpense>{
-  var _enteredTitle = '';
+  // var _enteredTitle = '';
 
-  void _saveTitleInput(String inputValue){
-    _enteredTitle = inputValue;
+  // void _saveTitleInput(String inputValue){
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleController = TextEditingController(); 
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {  // need to dispose all the text editing controllers we create, otherwise it will crash the application.
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,17 +33,33 @@ class _NewExpenseState extends State<NewExpense>{
       child: Column( // we don't use ListView because we don't need to scroll, generate content dynamically
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            // onChanged: _saveTitleInput,
+            controller: _titleController, // for multiple fields we need multiple controllers
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
            children: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
             ElevatedButton(
               onPressed: (){
-                print(_enteredTitle);
+                print(_titleController.text);
+                print(_amountController.text);
               }, 
               child: const Text('Save Expense'),
             ),
