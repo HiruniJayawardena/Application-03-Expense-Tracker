4771c2ec2c8e4db035c15a38439d6588d4127f84
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-09-26 09:56:14 +0530

    Color theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+// "k" used as a convention for global variables
+var kColorScheme  = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 36, 7, 37));
+
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        // scaffoldBackgroundColor: Color.fromARGB(255, 244, 174, 247),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        )
+      ),
+      home: const Expenses(),
     ),
   );
 }
