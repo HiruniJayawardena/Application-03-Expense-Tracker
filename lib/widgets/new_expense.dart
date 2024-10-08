b2207c797a1838f3ac79919a629ad18adc6af82f
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-08 10:09:03 +0530

    keyboardSpace added to padding

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -86,9 +86,9 @@ class _NewExpenseState extends State<NewExpense>{
 
   @override
   Widget build(BuildContext context) {
-    MediaQuery.of(context).viewInsets.bottom;
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
     return Padding( 
-      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
+      padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
       child: Column( // we don't use ListView because we don't need to scroll, generate content dynamically
         children: [
           TextField(
